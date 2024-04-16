de97469a91e7c8a16218cadc808fadb34d58c0e1
Ebtehal <Ebtehal.Matar@gmail.com>
2024-04-16 13:55:22 +0300

    results 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,15 +30,6 @@ public_users.get('/',function (req, res) {
   return res.status(200).send(books);
 });
 
-axios.get('/')
-.then(response => {
-  res.status(200).send(response.data);
-})
-.catch(error => {
-  console.error('Error fetching data:', error);
-  res.status(500).send('An error occurred while fetching data');
-});
-
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
@@ -107,5 +98,87 @@ public_users.get('/review/:isbn',function (req, res) {
   
     return res.status(200).send(book.reviews);
 });
+
+
+
+
+//10
+function getBookList() {
+  return new Promise((resolve, reject) => resolve(books));
+}
+
+// Get the book list available
+public_users.get('/', function (req, res) {
+  getBookList().then(
+    (bk) => res.send(JSON.stringify(bk, null, 4)),
+    (error) => res.send("denied")
+  );
+});
+
+//11
+function getFromISBN(isbn) {
+  let book_ = books[isbn];
+  return new Promise((resolve, reject) => {
+    if (book_) {
+      resolve(book_);
+    } else {
+      reject("Unable to find book!");
+    }
+  });
+}
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  getFromISBN(isbn).then(
+    (bk) => res.send(JSON.stringify(bk, null, 4)),
+    (error) => res.send(error)
+  );
+});
+
+//12
+function getFromAuthor(author) {
+  let output = [];
+  return new Promise((resolve, reject) => {
+    for (var isbn in books) {
+      let book_ = books[isbn];
+      if (book_.author === author) {
+        output.push(book_);
+      }
+    }
+    resolve(output);
+  });
+}
+
+// Get book details based on author
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  getFromAuthor(author).then(
+    (result) => res.send(JSON.stringify(result, null, 4))
+  );
+});
+
+//13
+function getFromTitle(title) {
+  let output = [];
+  return new Promise((resolve, reject) => {
+    for (var isbn in books) {
+      let book_ = books[isbn];
+      if (book_.title === title) {
+        output.push(book_);
+      }
+    }
+    resolve(output);
+  });
+}
+
+// Get all books based on title
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  getFromTitle(title).then(
+    (result) => res.send(JSON.stringify(result, null, 4))
+  );
+});
+
 
 module.exports.general = public_users;
